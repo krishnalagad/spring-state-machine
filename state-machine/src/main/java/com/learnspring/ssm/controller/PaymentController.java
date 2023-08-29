c3c28a11397db61991f4e64bf1851a09272431d2
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-29 20:56:03 +0530

    - -Autowired required dependency

--- state-machine/src/main/java/com/learnspring/ssm/controller/PaymentController.java
+++ state-machine/src/main/java/com/learnspring/ssm/controller/PaymentController.java
@@ -1,9 +1,14 @@
 package com.learnspring.ssm.controller;
 
+import com.learnspring.ssm.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
 }
