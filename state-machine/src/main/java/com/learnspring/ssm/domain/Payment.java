7d6e45536baf79396d8e486d889f33500d6e6990
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-29 13:18:37 +0530

    - dump state, event and repository

---
+++ state-machine/src/main/java/com/learnspring/ssm/domain/Payment.java
@@ -0,0 +1,26 @@
+package com.learnspring.ssm.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState states;
+
+    private BigDecimal amount;
+}
---
+++ state-machine/src/main/java/com/learnspring/ssm/domain/PaymentEvent.java
@@ -0,0 +1,5 @@
+package com.learnspring.ssm.domain;
+
+public enum PaymentEvent {
+    PRE_AUTHORIZED, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZED, AUTH_APPROVED, AUTH_DECLINED
+}
